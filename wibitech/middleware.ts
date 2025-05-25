a4c9aec9c939ff64cc15a5957b48df48652b3318
mittous <issmaimit123@gmail.com>
2025-05-25 10:30:59 +0100

    clean code

--- wibitech/middleware.ts
+++ wibitech/middleware.ts
@@ -2,16 +2,12 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
-  // If the request is for the root path
   if (request.nextUrl.pathname === '/') {
-    // Redirect to the login page
     return NextResponse.redirect(new URL('/login', request.url));
   }
-  
   return NextResponse.next();
 }
 
-// Configure middleware to run only on the home route
 export const config = {
   matcher: '/',
 }; 
